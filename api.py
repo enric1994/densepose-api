6edc9a7a410dde40cff1ba45d977a94b19df9a66
Enric Moreu <enric.moreu.filella@gmail.com>
2019-12-06 14:05:16 +0000

    Delete comments

--- api.py
+++ api.py
@@ -16,7 +16,6 @@ app.debug = True
 @app.route('/', methods=['POST'])
 def process_files():
 
-	# Receive the different files and store them in an array
 	received_images = []
 	images = request.files.to_dict()
 	for image in images:
@@ -28,20 +27,13 @@ def process_files():
 		received_images.append(imageRGB)
 
 	# Do some operation using the received files and the parameters
-	# e.g. blend the two images using the param1 value (0.5)
-	# alpha = float(request.args.get('param1'))
-	# blended = alpha * received_images[0] + (1 - alpha) * received_images[1]
 	imsave('/densepose-api/tmp/tmp_result.png', received_images[0])
 
 	logging.info('Applying densepose...')
 	os.system('python2 /densepose/tools/infer_simple.py     --cfg /densepose/configs/DensePose_ResNet101_FPN_s1x-e2e.yaml     --output-dir /densepose-api/tmp     --image-ext png     --wts https://dl.fbaipublicfiles.com/densepose/DensePose_ResNet101_FPN_s1x-e2e.pkl     /densepose-api/tmp/tmp_result.png')	
 
-	# Return one file and more parameters
+	# Return one file
 	resp = send_file(open('/densepose-api/tmp/tmp_result_IUV.png'), mimetype='image/png')
-	# resp.headers['key1'] = 'value1'
-	# resp.headers['key2'] = 'value2'
 	return resp
 
 app.run(host="0.0.0.0", port=5000)
-
-#python2 /densepose/tools/infer_simple.py     --cfg /densepose/configs/DensePose_ResNet101_FPN_s1x-e2e.yaml     --output-dir /densepose-api/tmp     --image-ext png     --wts https://dl.fbaipublicfiles.com/densepose/DensePose_ResNet101_FPN_s1x-e2e.pkl     /densepose-api/tmp/tmp_result.png
